da1f46cc3e00da4ded9e3f8ecb7ce2053e62faf2
Alex Jin <aojin91@gmail.com>
2024-07-08 16:55:49 -0800

    loader issues resolved

--- App.tsx
+++ App.tsx
@@ -1,24 +1,24 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Platform, Text } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
+// Import Tailwind CSS
+import 'tailwindcss/tailwind.css';
 
-const AppNavigator = React.lazy(() =>
-  Platform.select({
-    web: () => import('./src/navigation/AppNavigator.web'),
-    default: () => import('./src/navigation/AppNavigator.native'),
-  })(),
-);
+/* eslint-disable @typescript-eslint/no-var-requires */
+const AppNavigator = Platform.select({
+  web: () => require('./src/navigation/AppNavigator.web').default,
+  default: () => require('./src/navigation/AppNavigator.native').default,
+})();
+/* eslint-enable @typescript-eslint/no-var-requires */
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Suspense fallback={<Text>Loading...</Text>}>
-        <AppNavigator />
-      </Suspense>
+      <AppNavigator />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -26,3 +26,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
 });
+
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-
-  const isDevelopment = process.env.NODE_ENV === 'development';
-  console.log('Babel environment:', process.env.NODE_ENV);
-  console.log('Is Development:', isDevelopment);
-
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'nativewind/babel',
-      isDevelopment && 'react-refresh/babel',
-    ].filter(Boolean),
+    plugins: ['nativewind/babel'],
   };
 };
